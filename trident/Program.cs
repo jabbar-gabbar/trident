5469a23b4247e053372284cfc20080fd713143fa
jabbar-gabbar <372647+jabbar-gabbar@users.noreply.github.com>
2019-03-19 17:19:07 -0500

    added configuration data and exception handling

--- trident/Program.cs
+++ trident/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.IO;
 using log4net;
+using System.Configuration;
 //using log4net.Config;
 
 namespace trident
@@ -14,34 +15,61 @@ namespace trident
     {
         static List<Settings> syncSettings;
         static ILog log = LogManager.GetLogger(typeof(Program));
-
+        static string settingFileName = string.Empty;
         static void Main(string[] args)
         {
-            //XmlConfigurator.Configure(); // optional way to load log4net config from app.config
-            if (log.IsInfoEnabled)
+            try
             {
-                log.Info("Starting trident backup...");
-            }
-            // load settings.json file and retrieve sync settings
-            // iterate through each sync setting item and start sync.
+                //XmlConfigurator.Configure(); // optional way to load log4net config from app.config
+                if (log.IsInfoEnabled)
+                {
+                    log.Info("Starting trident backup...");
+                }
+                // Check for configuration are in proper state. 
+                readAllConfig();
+                // load settings.json file and retrieve sync settings
+                // iterate through each sync setting item and start sync.
 
-            loadSyncSettings();
-            if (syncSettings == null || syncSettings.Count == 0)
+                loadSyncSettings();
+                if (syncSettings == null || syncSettings.Count == 0)
+                {
+                    log.Warn("syncSettings object is null or has zero sync settings.  Please check settings.json file content");
+                    return;
+                }
+
+                Sync sync = new Sync(syncSettings);
+                sync.start();
+
+                log.Info("Completing backup!!!");
+            }
+            catch (Exception ex)
             {
-                log.Warn("syncSettings object is null or has zero sync settings.  Please check settings.json file content");
-                return;
+                log.Error(string.Format("Error: {0}", ex.Message), ex);
             }
+        }
 
-            Sync sync = new Sync(syncSettings);
-            sync.start();
-
-            log.Info("Completing backup!!!");
+        private static void readAllConfig()
+        {
+            try
+            {
+                settingFileName = ConfigurationManager.AppSettings["SettingsFileNameasdfasdf"];
+                if (string.IsNullOrEmpty(settingFileName))
+                {
+                    throw new SettingsPropertyNotFoundException("Cannot find SettingsFileName property in config file.");
+                }
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                log.Error(ex.Message, ex);
+                throw new ConfigurationErrorsException("Error occured during readAllConfig(). ", ex);
+            }
         }
 
         static void loadSyncSettings()
         {
+            // TODO: add file exists checks. and Exception block. 
             string strSettings = string.Empty;
-            using (StreamReader r = new StreamReader(AppContext.BaseDirectory + "\\settings.json"))
+            using (StreamReader r = new StreamReader(AppContext.BaseDirectory + "\\" + settingFileName))
             {
                 strSettings = r.ReadToEnd();
             }
